ac1eae2af35018403a43e54941e9442879246f33
Team-testum <testers.testum@gmail.com>
2024-02-20 00:04:57 +0000

    Currys PC Main Navigation

--- Functionaltest/Playwright/tests/CurriesPc.spec.ts
+++ Functionaltest/Playwright/tests/CurriesPc.spec.ts
@@ -6,3 +6,8 @@ test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Currys | Washing Machines, Laptops, TVs, Consoles/);
 });
+
+test('main navigation', async ({ page }) => {
+    await page.goto('https://www.currys.co.uk/');
+    await expect(page).toHaveURL('https://www.currys.co.uk');
+  });
